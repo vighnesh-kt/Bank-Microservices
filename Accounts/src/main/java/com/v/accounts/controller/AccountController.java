d1cf4ae3ce08033e89b34829cff358a2327172b0
Vighnesh Tathare <v42069@yahoo.com>
2025-03-18 00:23:11 +0530

    add

---
+++ Accounts/src/main/java/com/v/accounts/controller/AccountController.java
@@ -0,0 +1,17 @@
+package com.v.accounts.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class AccountController {
+	
+	@GetMapping("/home")
+	public String getMethodName() {
+		return "Hello world";
+	}
+	
+
+}
