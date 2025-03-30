655c4512a9dba624132c96a007f1960ab8a22044
Vighnesh Tathare <v42069@yahoo.com>
2025-03-30 15:09:09 +0530

    add

--- Cards/src/main/java/com/v/cards/controller/CardsController.java
+++ Cards/src/main/java/com/v/cards/controller/CardsController.java
@@ -26,8 +26,8 @@ import com.v.cards.service.ICardsService;
  */
 
 @Tag(
-        name = "CRUD REST APIs for Cards in EazyBank",
-        description = "CRUD REST APIs in EazyBank to CREATE, UPDATE, FETCH AND DELETE card details"
+        name = "CRUD REST APIs for Cards in Bank",
+        description = "CRUD REST APIs in Bank to CREATE, UPDATE, FETCH AND DELETE card details"
 )
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -39,7 +39,7 @@ public class CardsController {
 
     @Operation(
             summary = "Create Card REST API",
-            description = "REST API to create new Card inside EazyBank"
+            description = "REST API to create new Card inside Bank"
     )
     @ApiResponses({
             @ApiResponse(
--- Loans/src/main/java/com/v/loans/controller/LoansController.java
+++ Loans/src/main/java/com/v/loans/controller/LoansController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.v.loans.dto.LoansDto;
@@ -83,7 +82,7 @@ public class LoansController {
 	    	return ResponseEntity.status(updateLoan.getStatusCode()).body(updateLoan);
 	    }
 	    
-	    @DeleteMapping("fetch")
+	    @DeleteMapping("delete")
 	    public ResponseEntity<?> deleteLoan(@RequestParam String mobileNumber) {
 	    	ResponseStructure<?> deleteLoan = iLoansService.fetchLoan(mobileNumber);
 	    	return ResponseEntity.status(deleteLoan.getStatusCode()).body(deleteLoan);
