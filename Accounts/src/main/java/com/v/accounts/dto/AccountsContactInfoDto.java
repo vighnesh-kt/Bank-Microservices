634c4df524eca68612480a86798980dde3a10e9b
Vighnesh Tathare <v42069@yahoo.com>
2025-05-12 00:40:06 +0530

    add

--- Accounts/src/main/java/com/v/accounts/dto/AccountsContactInfoDto.java
+++ Accounts/src/main/java/com/v/accounts/dto/AccountsContactInfoDto.java
@@ -2,12 +2,20 @@ package com.v.accounts.dto;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 import java.util.Map;
 
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class AccountsContactInfoDto {
 
+	private String message;
+	private Map<String, String> contactDetails;
+	private List<String> onCallSupport;
 }
 
